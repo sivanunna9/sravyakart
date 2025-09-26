e72eebf40210e0ab46ac093aeb2e3bbae5b252d8
nns sravya <nnssravya@nnss-MacBook-Air.local>
2025-09-26 17:53:56 +0530

    added install_maven.sh

---
+++ install_maven.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update -y
+
+# Install software-properties-common (for add-apt-repository)
+sudo apt install -y software-properties-common
+
+# Add Ansible PPA
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+# Install Ansible
+sudo apt install -y ansible
+
+# Verify installation
+ansible --version
+
